495a8747a94ab9ca918f94810f9b8d1680a647cb
sirjan tamang <sirjantmg99@gmail.com>
2022-04-21 07:17:12 +0545

    initial load

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CreateTable from './components/CreateTable';
+import './App.css'
+
+
+export default function App(){
+
+  return (
+    <div className="AppComponent">
+      <CreateTable />
+    </div>
+  )
+}
